67044f678642de3a279c2cdbac34bf00197df3e9
Yvette Pettinelli <ipettinelli@att.net>
2017-10-19 10:13:41 -0500

    Add edit and update JavaScript views.

--- app/controllers/user_tasks_controller.rb
+++ app/controllers/user_tasks_controller.rb
@@ -1,5 +1,5 @@
 class UserTasksController < ApplicationController
-  before_action :all_tasks, only: [:index, :create]
+  before_action :all_tasks, only: [:index, :create, :update]
   before_action :set_user_task, only: [:show, :edit, :update, :destroy]
 
   # GET /user_tasks
@@ -46,9 +46,11 @@ class UserTasksController < ApplicationController
     respond_to do |format|
       if @user_task.update(user_task_params)
         format.html { redirect_to @user_task, notice: 'User task was successfully updated.' }
+        format.js {}
         format.json { render :show, status: :ok, location: @user_task }
       else
         format.html { render :edit }
+        format.js { render :edit }
         format.json { render json: @user_task.errors, status: :unprocessable_entity }
       end
     end
