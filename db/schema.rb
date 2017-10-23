c8a23174ad510c814e43bf5bbf0984e1f4dbc56e
Yvette Pettinelli <ipettinelli@att.net>
2017-10-23 09:05:44 -0500

    Setup user model to use for omniauth-facebook.

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171019134703) do
+ActiveRecord::Schema.define(version: 20171023135316) do
 
   create_table "user_tasks", force: :cascade do |t|
     t.string "description"
     t.date "due"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
