ef2c43b0c00afe656a9d037a1d40d52327ab0119
Yvette Pettinelli <ipettinelli@att.net>
2017-10-23 09:25:09 -0500

    Add current user and setup sessions controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :user_tasks
 
+  get '/auth/:provider/callback' => 'sessions#create'
+  get '/signout/' => 'sessions#destroy', as: :signout
+
   root 'user_tasks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
